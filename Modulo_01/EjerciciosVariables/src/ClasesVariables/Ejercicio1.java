b4b3b3587b2dc325dadf285394d1d66e4bbe1a38
Erikajpb <erikajpinedab@gmail.com>
2024-08-02 09:09:25 -0500

    Ejercicios variables

---
+++ Modulo_01/EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,25 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+
+		String nombrePersona = "Juan Perez";
+
+		double x = 12.10;
+
+		final double valorDolar = 3.900;
+
+		float pi = 3.1415927F;
+
+		Double euler = 2.718281828459045;
+
+		System.out.println("Nombre: " + nombrePersona);
+		System.out.println("x: " + x);
+		System.out.println("Valor del dólar: " + valorDolar);
+		System.out.println("Pi (7 decimales): " + pi);
+		System.out.println("Euler (15 decimales): " + euler);
+
+	}
+
+}
---
+++ Modulo_01/EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,39 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		Double a = 8.0;
+		char b = 'A';
+		String c = "Colombia";
+		String d = "A";
+		float e = 74548F;
+		int numero1 = 8;
+		String nombrePersona = "Juan Perez";
+		double edad = 25;
+		String altura = "1.71";
+		double alturaReal = Double.parseDouble(altura);
+		int var = 96;
+		int f = 98;
+		int x = f;
+		String y = "25";
+		String g = y;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(numero1);
+		System.out.println(nombrePersona);
+		System.out.println(edad);
+		System.out.println(alturaReal);
+		System.out.println(var);
+		System.out.println(f);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(g);
+	}
+
+}
